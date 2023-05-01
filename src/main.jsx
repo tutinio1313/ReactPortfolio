95f957485da35d2e3c5d58ed0f636abebfaffa0e
Andres Rossini <tutinio1313@gmail.com>
2023-05-01 19:31:51 -0300

    Added title internalization.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./TailwindStyle.css";
+import SearchText from './InternalizationHandler.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+document.title = SearchText("Title");
